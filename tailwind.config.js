144c02503c97d0cf7ffc049e8c0de69b76d6a6de
pilefort <pilefort2020@gmail.com>
2022-04-10 03:50:55 +0900

    feat スマホ用のヘッダーを追加しました

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,27 @@
 module.exports = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fadeIn: 'fadeIn 0.5s ease-in forwards',
+        slideIn: 'slideIn 0.5s forwards',
+        slideOut: 'slideOut 0.5s forwards',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+        slideIn: {
+          '0%': { opacity: 0, transform: 'translateX(100%)' },
+          '100%': { opacity: 1, transform: 'translateX(0%)' },
+        },
+        slideOut: {
+          '0%': { opacity: 1, transform: 'translateX(0%)' },
+          '100%': { opacity: 0, transform: 'translateX(100%)' },
+        },
+      },
+    },
   },
   plugins: [],
 }
