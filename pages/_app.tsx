37b35fee9e326d6292cae0686fba659c1fb26cf3
kusakabe <kusakabe.takuya@alleeks.com>
2022-04-10 07:46:40 +0900

    feat フッターを追加しました

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Header from '../src/components/Layouts/Header'
+import Footer from '../src/components/Layouts/Footer'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Header />
-      <Component {...pageProps} />
+      <div className="h-[calc(100vh-(80px+80px))]">
+        <Component {...pageProps} />
+      </div>
+      <Footer />
     </>
   )
 }
