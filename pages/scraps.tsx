dbc5b523faea138380a2e933fad53a2b8f164ff2
kusakabe <kusakabe.takuya@alleeks.com>
2022-04-14 22:35:18 +0900

    feat Scrapsページのレイアウト部分を作りました

--- pages/scraps.tsx
+++ pages/scraps.tsx
@@ -2,6 +2,13 @@ import data from '../fetchData/demo/demoWith1Scrap.json'
 import scrapsListsData from '../fetchData/demo/demoWithScrapLists.json'
 
 import { NextPage, InferGetStaticPropsType } from 'next'
+import { useState } from 'react'
+import Link from 'next/link'
+import { utcToJst } from '../src/libs/date'
+import Image from 'next/image'
+
+import TagIcon from '../public/assets/Tag.svg'
+import LinkIcon from '../public/assets/Link.svg'
 
 type CommonMicroCMSResponse = {
   createdAt?: string
@@ -14,7 +21,7 @@ type ContentType = CommonMicroCMSResponse & {
   id: string
   date: string
   createdAt: string
-  highlight?: string
+  highlight: string
   scraps: (CommonMicroCMSResponse & {
     id: string
     updatedAt: string
@@ -41,44 +48,116 @@ type ContentType = CommonMicroCMSResponse & {
 
 type ScrapsListType = {
   id: string
+  date?: string
   createdAt: string
 }[]
 
-const ScrapsIndexPage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
-  content: { date, createdAt, scraps, highlight },
-  scrapsLists,
-}) => {
+const ScrapsIndexPage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({ content: { scraps, highlight }, scrapsLists }) => {
+  const [isHover, setIsHover] = useState(false)
+
   return (
     <div className="p-[24px]">
-      <div>scrapリスト一覧</div>
-      {scrapsLists.map(({ id, createdAt }) => {
-        return <div key={id}>{createdAt}</div>
-      })}
-      <div>Scrapページです！</div>
-      <div>日付: {date ? date : createdAt}</div>
-      <div>ハイライト: {highlight}</div>
-      {scraps.map(({ id, title, tags, updatedAt, body, links }) => {
-        return (
-          <div key={id}>
-            <div>{title}</div>
-            <div>{tags?.join('')}</div>
-            <div>{updatedAt}</div>
-            {links?.map(({ url }, index) => {
-              return <div key={index}>{url}</div>
-            })}
-            <div>
-              {body.map(({ content }, index) => {
-                return (
-                  <div
-                    key={index}
-                    dangerouslySetInnerHTML={{ __html: content }}
-                  />
-                )
-              })}
+      <select
+        className="text-[24px]"
+        name="scraps"
+      >
+        {scrapsLists.map(({ id, createdAt, date }) => {
+          const dateTime = utcToJst({ date: date || createdAt })
+
+          return (
+            <option
+              key={id}
+              value={id}
+            >
+              {dateTime}
+            </option>
+          )
+        })}
+      </select>
+
+      <div>
+        <div className="mt-[24px] w-[120px] rounded-[10px_10px_0_0px] border-[5px_5px_0_5px] border-[#1ED3C6] text-center text-[18px]">
+          ハイライト
+        </div>
+        <div
+          className={'border-[5px] border-[#1ED3C6] p-[8px]'}
+          dangerouslySetInnerHTML={{ __html: highlight }}
+        />
+      </div>
+      <div className="mt-[64px]">
+        {scraps.map(({ id, title, tags, updatedAt, body, links }) => {
+          const date = utcToJst({ date: updatedAt })
+
+          return (
+            <div
+              className="mt-[52px]"
+              key={id}
+            >
+              <div className="text-[32px]">{title}</div>
+              <div>{date}</div>
+              {tags &&
+                tags.map(({ id, title, description }) => {
+                  return (
+                    <div
+                      key={id}
+                      className="mr-[8px] flex items-center"
+                    >
+                      <Image
+                        src={TagIcon}
+                        alt="tag"
+                      />
+                      <span
+                        className="ml-[4px]"
+                        onMouseOver={() => setIsHover(true)}
+                        onMouseLeave={() => setIsHover(false)}
+                      >
+                        {title}
+                      </span>
+                      {isHover && <div dangerouslySetInnerHTML={{ __html: description }} />}
+                    </div>
+                  )
+                })}
+              <div className="mt-[16px]">
+                {body.map(({ content, fieldId }, index) => {
+                  return fieldId === 'plainText' ? (
+                    <div>MarkdownToHTMLで表示する部分</div>
+                  ) : (
+                    <div
+                      key={index}
+                      dangerouslySetInnerHTML={{ __html: content }}
+                    />
+                  )
+                })}
+              </div>
+              {links &&
+                links.map(({ url }, index) => {
+                  return (
+                    <div
+                      key={index}
+                      className="mt-[8px] flex"
+                    >
+                      <div className="mt-[2px] h-[24px] w-[60px]">
+                        <Image
+                          src={LinkIcon}
+                          width={24}
+                          height={24}
+                          alt="link"
+                        />
+                      </div>
+                      <Link
+                        href={url}
+                        key={index}
+                        passHref
+                      >
+                        <a className="ml-[8px] text-[#10AFA4]">{url}</a>
+                      </Link>
+                    </div>
+                  )
+                })}
             </div>
-          </div>
-        )
-      })}
+          )
+        })}
+      </div>
     </div>
   )
 }
