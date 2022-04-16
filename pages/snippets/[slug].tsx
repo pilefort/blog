cab3b179ef31f2e8e0e5366103f2e2b97ce0c8cb
kusakabe <kusakabe.takuya@alleeks.com>
2022-04-17 01:32:00 +0900

    feat Snippets詳細ページのデータ取得部分を追加しました

---
+++ pages/snippets/[slug].tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+import { NextPage, InferGetStaticPropsType } from 'next'
+
+import { join } from 'path'
+import MDX from '@mdx-js/runtime'
+
+import { getAllContentPaths, getContentBySlug } from '../../src/libs/getContentsFromMdx'
+
+import { CodeBlock } from '../../src/components/Mdx/CodeBlock'
+import { CustomImage } from '../../src/components/Mdx/CustomImage'
+
+const SnippetDetailsPage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({ title, tags, date, content }) => {
+  const components = {
+    code: CodeBlock,
+    img: CustomImage,
+  }
+
+  return (
+    <div>
+      <div>{title}</div>
+      <div>{tags}</div>
+      <div>{date}</div>
+      <MDX components={components}>{content}</MDX>
+    </div>
+  )
+}
+
+export const getStaticPaths = async () => {
+  const paths = await getAllContentPaths({ target: 'snippets' })
+
+  return { paths, fallback: false }
+}
+
+export const getStaticProps = ({ params }: { params: { slug: string } }) => {
+  const fullFilePath = join(process.cwd(), '/contents/snippets/', params.slug + '.mdx')
+
+  const contents = getContentBySlug(fullFilePath, ['title', 'content', 'tags', 'date'])
+
+  const { title, content, tags, date } = contents
+
+  return {
+    props: {
+      title,
+      content,
+      tags,
+      date,
+    },
+  }
+}
+
+export default SnippetDetailsPage
