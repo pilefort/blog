0b2f4e19a8cef4a936adb482a8bd44fa83b620e2
kusakabe <kusakabe.takuya@alleeks.com>
2022-04-17 23:50:15 +0900

    feat favicon刷新、metadataを整備しました

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,17 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Header from '../src/components/Layouts/Header'
 import Footer from '../src/components/Layouts/Footer'
+import { CustomHead } from '../src/components/Head/CustomHead'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Header />
+      {/* TODO: ある程度完成したら内容を充実させる */}
+      <CustomHead
+        title=""
+        description={'現在開発中の技術ブログです'}
+      />
       <div className="min-h-[calc(100vh-(80px+80px))]">
         <Component {...pageProps} />
       </div>
---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+const MyDocument = () => {
+  return (
+    <Html lang="ja">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
+
+export default MyDocument
