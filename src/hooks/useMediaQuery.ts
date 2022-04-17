7ed0aa25406955fdbd992bb46facaf5ba2998231
pilefort <pilefort2020@gmail.com>
2022-04-18 00:49:16 +0900

    refactoirng メディアクエリに関するカスタムフックを切り出しました

---
+++ src/hooks/useMediaQuery.ts
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+
+export const useMediaQuery = () => {
+  const [isDesktop, setIsDesktop] = useState(true)
+  const router = useRouter()
+
+  useEffect(() => {
+    if (!router.isReady) return
+
+    window.innerWidth >= 900 ? setIsDesktop(true) : setIsDesktop(false)
+  }, [router.isReady])
+
+  return isDesktop
+}
