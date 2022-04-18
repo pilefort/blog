62010993d9cb37ef47218c8dbf3fdbfa5a106b4d
pilefort <pilefort2020@gmail.com>
2022-04-19 00:18:47 +0900

    update Codeblockでウィンドウサイズが小さいときにoverflowするようにしました

--- src/components/Mdx/CodeBlock.tsx
+++ src/components/Mdx/CodeBlock.tsx
@@ -1,42 +1,51 @@
 import Highlight, { defaultProps, Language } from 'prism-react-renderer'
-import github from 'prism-react-renderer/themes/github'
+import github from 'prism-react-renderer/themes/nightOwl'
 import React from 'react'
 
 export const CodeBlock = ({ children, className }: { children: string; className: string }) => {
   const language: string = className ? className.replace(/language-/, '') : 'javascript'
 
+  // cf. https://github.com/FormidableLabs/prism-react-renderer
   return (
-    <Highlight
-      {...defaultProps}
-      code={children}
-      language={language as Language}
-      theme={github}
-    >
-      {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre
-          className={className}
-          style={{ ...style, padding: '16px', marginBottom: '32px', fontSize: '20px' }}
-        >
-          {tokens.map((line, i) => (
-            <div
-              key={i}
-              {...getLineProps({ line, key: i })}
-            >
-              <div style={{ display: 'flex' }}>
-                <div>{i + 1}</div>
-                <div style={{ marginLeft: '16px' }}>
-                  {line.map((token, key) => (
-                    <span
-                      key={key}
-                      {...getTokenProps({ token, key })}
-                    />
-                  ))}
+    <div className="my-[16px]">
+      <Highlight
+        {...defaultProps}
+        code={children}
+        language={language as Language}
+        theme={github}
+      >
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <pre
+            className={`${className} mb-[32px] overflow-auto p-[16px] text-[20px] sm:text-[15px]`}
+            style={{ ...style }}
+          >
+            {tokens.map((line, index) => {
+              const lineProps = getLineProps({ line, key: index })
+
+              return (
+                <div
+                  key={lineProps.key}
+                  {...lineProps}
+                >
+                  <div className="flex">
+                    <div>{index + 1}</div>
+                    <div className="ml-[16px]">
+                      {line.map((token, index) => {
+                        return (
+                          <span
+                            key={index}
+                            {...getTokenProps({ token, index })}
+                          />
+                        )
+                      })}
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-          ))}
-        </pre>
-      )}
-    </Highlight>
+              )
+            })}
+          </pre>
+        )}
+      </Highlight>
+    </div>
   )
 }
