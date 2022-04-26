93fc53afe7c6734d6874187c3f12d15b13cceea8
kusakabe <kusakabe.takuya@alleeks.com>
2022-04-27 00:30:33 +0900

    fix Desktop, SPを_appl.tsxで切り替えるようにしました

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,37 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Header from '../src/components/Layouts/Header'
 import Footer from '../src/components/Layouts/Footer'
 import { CustomHead } from '../src/components/Head/CustomHead'
+import { useMediaQuery } from '../src/hooks/useMediaQuery'
+import { DesktopHeader } from '../src/components/Layouts/Header/DesktopHeader'
+import { SPHeader } from '../src/components/Layouts/Header/SP/SPHeader'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const isDesktop = useMediaQuery()
+
   return (
     <>
-      <Header />
       {/* TODO: ある程度完成したら内容を充実させる */}
       <CustomHead
         title=""
         description={'現在開発中の技術ブログです'}
       />
-      <div className="min-h-[calc(100vh-(80px+80px+128px))]">
-        <Component {...pageProps} />
-      </div>
+      {isDesktop ? (
+        <>
+          <DesktopHeader />
+          <SPHeader />
+          <div className="min-h-[calc(100vh-(80px+80px+128px))]">
+            <Component {...pageProps} />
+          </div>
+        </>
+      ) : (
+        <>
+          <SPHeader />
+          <div className="min-h-[calc(100vh-(30px+80px+128px))]">
+            <Component {...pageProps} />
+          </div>
+        </>
+      )}
       <Footer />
     </>
   )
