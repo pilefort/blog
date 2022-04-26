31771940b03b1ec37a335152c31522cf29adbd2a
pilefort <pilefort2020@gmail.com>
2022-04-27 00:31:47 +0900

    update toggleメニューに背景追加

--- src/components/Layouts/Header/SP/ToggleMenu.tsx
+++ src/components/Layouts/Header/SP/ToggleMenu.tsx
@@ -1,4 +1,4 @@
-import { MouseEventHandler } from 'react'
+import { MouseEvent, MouseEventHandler } from 'react'
 
 import Image from 'next/image'
 import Link from 'next/link'
@@ -6,43 +6,55 @@ import Link from 'next/link'
 import AccordionCloseIcon from '../../../../../public/assets/AccordionClose.svg'
 
 export const ToggleMenu = ({ toggleClass, setToggleCloseClass }: { toggleClass: string; setToggleCloseClass: MouseEventHandler<HTMLDivElement> }) => {
+  const stopPropagationHandler = (event: MouseEvent<HTMLDivElement>) => {
+    event.stopPropagation()
+  }
+
   return (
-    <div className={`fixed right-0 z-10 h-[100vh] w-[108px] ${toggleClass} bg-[#104359]`}>
+    <div
+      className={`${toggleClass} fixed right-0 z-10 h-screen w-screen bg-black bg-opacity-[80%]`}
+      onClick={setToggleCloseClass}
+    >
       <div
-        className="absolute right-0 p-[8px]"
-        onClick={setToggleCloseClass}
+        className={`fixed right-0 z-10 h-[100vh] w-[108px] ${toggleClass} bg-[#104359]`}
+        onClick={stopPropagationHandler}
       >
-        <Image
-          src={AccordionCloseIcon}
-          alt={'toggle close'}
-        />
-      </div>
-      <div className="flex h-[95%] flex-col justify-between p-[8px]">
-        <div className="mt-[24px] flex flex-col justify-between">
-          <Link
-            href="/notes"
-            passHref
-          >
-            <a className="mt-[16px] text-[white]">Notes</a>
-          </Link>
-          <Link
-            href="/scraps"
-            passHref
-          >
-            <a className="mt-[16px] text-[white]">Scraps</a>
-          </Link>
-          <Link
-            href="/snippets"
-            passHref
-          >
-            <a className="mt-[16px] text-[white]">Snippets</a>
-          </Link>
-          <Link
-            href="/works"
-            passHref
-          >
-            <a className="mt-[16px] text-[white]">Works</a>
-          </Link>
+        <div
+          className="absolute right-0 p-[8px]"
+          onClick={setToggleCloseClass}
+        >
+          <Image
+            src={AccordionCloseIcon}
+            alt={'toggle close'}
+          />
+        </div>
+        <div className="flex h-[95%] flex-col justify-between p-[8px]">
+          <div className="mt-[24px] flex flex-col justify-between">
+            <Link
+              href="/notes"
+              passHref
+            >
+              <a className="mt-[16px] text-[white]">Notes</a>
+            </Link>
+            <Link
+              href="/scraps"
+              passHref
+            >
+              <a className="mt-[16px] text-[white]">Scraps</a>
+            </Link>
+            <Link
+              href="/snippets"
+              passHref
+            >
+              <a className="mt-[16px] text-[white]">Snippets</a>
+            </Link>
+            <Link
+              href="/works"
+              passHref
+            >
+              <a className="mt-[16px] text-[white]">Works</a>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
