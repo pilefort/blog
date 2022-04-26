48c929ff882edf2b3223a72622405f5400faa88a
pilefort <pilefort2020@gmail.com>
2022-04-26 09:45:17 +0900

    update headerをコンポーネント分けしました

---
+++ src/components/Layouts/Header/SP/ToggleMenu.tsx
@@ -0,0 +1,50 @@
+import { MouseEventHandler } from 'react'
+
+import Image from 'next/image'
+import Link from 'next/link'
+
+import AccordionCloseIcon from '../../../../../public/assets/AccordionClose.svg'
+
+export const ToggleMenu = ({ toggleClass, setToggleCloseClass }: { toggleClass: string; setToggleCloseClass: MouseEventHandler<HTMLDivElement> }) => {
+  return (
+    <div className={`fixed right-0 z-10 h-[100vh] w-[108px] ${toggleClass} bg-[#104359]`}>
+      <div
+        className="absolute right-0 p-[8px]"
+        onClick={setToggleCloseClass}
+      >
+        <Image
+          src={AccordionCloseIcon}
+          alt={'toggle close'}
+        />
+      </div>
+      <div className="flex h-[95%] flex-col justify-between p-[8px]">
+        <div className="mt-[24px] flex flex-col justify-between">
+          <Link
+            href="/notes"
+            passHref
+          >
+            <a className="mt-[16px] text-[white]">Notes</a>
+          </Link>
+          <Link
+            href="/scraps"
+            passHref
+          >
+            <a className="mt-[16px] text-[white]">Scraps</a>
+          </Link>
+          <Link
+            href="/snippets"
+            passHref
+          >
+            <a className="mt-[16px] text-[white]">Snippets</a>
+          </Link>
+          <Link
+            href="/works"
+            passHref
+          >
+            <a className="mt-[16px] text-[white]">Works</a>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
