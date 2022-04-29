d09f25cbd59fef2761c60fe0295e553a75285e64
kusakabe <kusakabe.takuya@alleeks.com>
2022-04-29 19:52:08 +0900

    wip footerの有無によるロードの速さ確認

--- components/Layouts/Footer.tsx
+++ components/Layouts/Footer.tsx
@@ -24,18 +24,19 @@ const Footer = () => {
       {isRoute ? (
         <></>
       ) : (
-        <div className="h-auto bg-[#104359] text-white md:text-[24px]">
-          <div className="p-[16px] md:p-[80px]">
-            <div className="hr-with-title text-center md:w-[calc(100vw-160px)]">自己紹介</div>
-            <div className="mt-[24px] md:w-[calc(100vw-180px)]">
-              <Introduction />
-              <div className="mt-[64px] md:mt-[112px]">
-                <LatestActivity />
-              </div>
-            </div>
-            <Sitemap />
-          </div>
-        </div>
+        <></>
+        // <div className="h-auto bg-[#104359] text-white md:text-[24px]">
+        //   <div className="p-[16px] md:p-[80px]">
+        //     <div className="hr-with-title text-center md:w-[calc(100vw-160px)]">自己紹介</div>
+        //     <div className="mt-[24px] md:w-[calc(100vw-180px)]">
+        //       <Introduction />
+        //       <div className="mt-[64px] md:mt-[112px]">
+        //         <LatestActivity />
+        //       </div>
+        //     </div>
+        //     <Sitemap />
+        //   </div>
+        // </div>
       )}
     </div>
   )
