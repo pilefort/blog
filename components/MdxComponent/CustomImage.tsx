d1f0cdfe78be89d2a7ca66380786d54c427453a6
kusakabe <kusakabe.takuya@alleeks.com>
2022-04-29 19:18:20 +0900

    fix 画像をロード中の間はぼかすようにしました

--- components/MdxComponent/CustomImage.tsx
+++ components/MdxComponent/CustomImage.tsx
@@ -13,8 +13,8 @@ type ImagePropsType = {
 export const CustomImage = (props: ImagePropsType) => {
   const src = props.src.replace(/^\.\./, '')
 
-  const [isShowModal, setIsShowModal] = useState(false)
-  const [isShowImage, setIsShowImage] = useState(false)
+  const [isShowModal, setIsShowModal] = useState<boolean>(false)
+  const [isImageLoaded, setIsImageLoaded] = useState<boolean>(false)
   const stopPropagationHandler = (event: MouseEvent<HTMLDivElement>) => {
     event.stopPropagation()
 
@@ -23,48 +23,38 @@ export const CustomImage = (props: ImagePropsType) => {
   const [isDisableBookObi, setIsDisableBookObi] = useState(false)
   const closeModalHandler = () => setIsShowModal(false)
 
-  setTimeout(() => {
-    setIsShowImage(true)
-  }, 5000)
-
   return (
     <>
-      {isShowImage ? (
-        <>
+      <div
+        className="cursor-zoom-in"
+        onClick={() => setIsShowModal(true)}
+      >
+        <Image
+          alt={props.alt}
+          src={src}
+          width={props.width}
+          height={props.height}
+          className={`${isImageLoaded ? 'blur-none' : 'blur-sm'}`}
+          onLoadingComplete={() => setIsImageLoaded(true)}
+        />
+      </div>
+      {isShowModal && (
+        <div
+          className="fixed top-0 left-0 z-[100] flex h-screen w-screen items-center justify-center bg-black bg-opacity-60 transition duration-500 ease-out xl:scale-[1.5]"
+          onClick={closeModalHandler}
+        >
           <div
-            className="cursor-zoom-in"
-            onClick={() => setIsShowModal(true)}
+            className="relative"
+            onClick={stopPropagationHandler}
           >
             <Image
               alt={props.alt}
               src={src}
               width={props.width}
               height={props.height}
-              // placeholder="blur"
-              // blurDataURL={'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z/C/HgAGgwJ/lK3Q6wAAAABJRU5ErkJggg=='}
             />
           </div>
-          {isShowModal && (
-            <div
-              className="fixed top-0 left-0 z-[100] flex h-screen w-screen items-center justify-center bg-black bg-opacity-60 transition duration-500 ease-out xl:scale-[1.5]"
-              onClick={closeModalHandler}
-            >
-              <div
-                className="relative"
-                onClick={stopPropagationHandler}
-              >
-                <Image
-                  alt={props.alt}
-                  src={src}
-                  width={props.width}
-                  height={props.height}
-                />
-              </div>
-            </div>
-          )}
-        </>
-      ) : (
-        <div>画像読み込み中...</div>
+        </div>
       )}
     </>
   )
