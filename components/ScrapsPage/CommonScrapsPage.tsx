25c81ec904e36e34905fa1989483dc80549a69c5
pilefort <pilefort2020@gmail.com>
2022-05-02 22:44:19 +0900

    refactoring scraps page index

---
+++ components/ScrapsPage/CommonScrapsPage.tsx
@@ -0,0 +1,22 @@
+import scrapsListsData from '../../fetchData/scraps/demoWithScrapLists.json'
+
+import { Sidebar } from './Sidebar/Sidebar'
+import { Highlight } from './contents/Highlight'
+import { Contents } from './contents/Contents'
+
+import { ScrapsType } from '../../types/microCMS/Common'
+
+export const CommonScrapsPage = ({ dateTime, highlight, scraps }: { dateTime: string; highlight: string; scraps: ScrapsType }) => {
+  const { contents: scrapsLists } = scrapsListsData
+
+  return (
+    <div className="p-[24px] md:flex">
+      <Sidebar scrapsLists={scrapsLists} />
+      <div className="ml-[32px]">
+        <div className="text-[32px]">{dateTime}</div>
+        <Highlight highlight={highlight} />
+        <Contents scraps={scraps} />
+      </div>
+    </div>
+  )
+}
--- pages/scraps.tsx
+++ pages/scraps.tsx
@@ -1,18 +1,13 @@
-import scrapsListsData from '../fetchData/scraps/demoWithScrapLists.json'
-
 import { NextPage, InferGetStaticPropsType } from 'next'
 
-import { Sidebar } from '../components/ScrapsPage/Sidebar/Sidebar'
-import { Highlight } from '../components/ScrapsPage/contents/Highlight'
-import { Contents } from '../components/ScrapsPage/contents/Contents'
-
-import { ScrapsListType, ScrapsType } from '../types/microCMS/Common'
+import { ScrapsType } from '../types/microCMS/Common'
 import { getListContents } from '../utils/getContents'
 import { utcToJst } from '../libs/date'
 import { CustomHead } from '../components/MetaHead/CustomHead'
+import { CommonScrapsPage } from '../components/ScrapsPage/CommonScrapsPage'
 
-const ScrapsIndexPage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({ date, createdAt, highlight, scraps, scrapsLists }) => {
-  const postDate = utcToJst({ date: date || createdAt })
+const ScrapsIndexPage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({ date, createdAt, highlight, scraps }) => {
+  const dateTime = utcToJst({ date: date || createdAt })
 
   return (
     <>
@@ -20,20 +15,17 @@ const ScrapsIndexPage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> 
         title="最新のスクラップ"
         description="個人的に収集した情報をまとめています"
       />
-      <div className="p-[24px] md:flex">
-        <Sidebar scrapsLists={scrapsLists} />
-        <div>
-          <div className="text-[32px]">{postDate}</div>
-          <Highlight highlight={highlight} />
-          <Contents scraps={scraps} />
-        </div>
-      </div>
+      <CommonScrapsPage
+        scraps={scraps}
+        highlight={highlight}
+        dateTime={dateTime}
+      />
     </>
   )
 }
 
 export const getStaticProps: () => Promise<{
-  props: { date: string; createdAt: string; highlight: string; scraps: ScrapsType; scrapsLists: ScrapsListType }
+  props: { date: string; createdAt: string; highlight: string; scraps: ScrapsType }
 }> = async () => {
   const endpoint = 'scraps'
   const queries = {
@@ -49,15 +41,12 @@ export const getStaticProps: () => Promise<{
   })
   const { date, createdAt, highlight, scraps }: { date: string; createdAt: string; highlight: string; scraps: ScrapsType } = contents[0]
 
-  const { contents: scrapsLists } = scrapsListsData
-
   return {
     props: {
       date,
       createdAt,
       highlight,
       scraps,
-      scrapsLists,
     },
   }
 }
