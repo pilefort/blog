b1c9a7f5c74a8d7279c221795fb83d9c28f5b94b
kusakabe <kusakabe.takuya@alleeks.com>
2022-05-05 15:22:39 +0900

    update mdx

--- components/MdxComponent/Mdx.tsx
+++ components/MdxComponent/Mdx.tsx
@@ -5,6 +5,7 @@ import MDX from '@mdx-js/runtime'
 import { CodeBlock } from './CodeBlock'
 import { CustomImage } from '../CustomImage'
 import { H1, H2, P, List } from './Text'
+import Link from 'next/link'
 
 export const Mdx = ({ children }: { children: string }) => {
   const components = {
@@ -14,6 +15,19 @@ export const Mdx = ({ children }: { children: string }) => {
     img: CustomImage,
     p: P,
     li: List,
+    a: ({ children, href }: { children: string; href: string }) => (
+      <Link
+        href={href}
+        passHref
+      >
+        <a
+          target="_blank"
+          className="text-[#10AFA4]"
+        >
+          {children}
+        </a>
+      </Link>
+    ),
     Pending: ({ children }: { children: string }) => (
       <>
         <div className="text-center text-[80px] font-bold">準備中</div>
