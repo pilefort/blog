38ad35cd16541b87ca4e2cf12655b34d7ca28ec0
pilefort <pilefort2020@gmail.com>
2022-05-15 21:13:52 +0900

    feat RSSからブログ記事を取得するようにしました
    参考
    https://github.com/catnose99/team-blog-hub

---
+++ bin/fetchRssData.ts
@@ -0,0 +1,20 @@
+// #!/usr/bin/env ts-node
+import { createJsonFile } from './common/createJsonFile'
+import Parser from 'rss-parser'
+
+const folder = 'fetchData/notes'
+const dataFileName = 'zenn.json'
+
+export const main = async () => {
+  const { items } = await new Parser().parseURL('https://zenn.dev/pilefort/feed')
+
+  const blogs = items.map(({ title, link, pubDate, content }) => ({ title, link, pubDate, content }))
+
+  createJsonFile({
+    folder,
+    filename: folder + '/' + dataFileName,
+    content: JSON.stringify(Array.from(blogs)),
+  })
+}
+
+main()
