54d0eb9a0b9822840f04c82abdfde448f3a0384d
kusakabe <kusakabe.takuya@alleeks.com>
2022-06-12 10:16:46 +0900

    fix コードスタイルを修正

--- next.config.mjs
+++ next.config.mjs
@@ -34,15 +34,15 @@ export default withPWA({
       config.resolve.fallback.fs = false
     }
 
-    return config;
+    return config
   },
   // TODO: total blocking timeは短くなるが、next/linkの動きが悪くなる (クリックしても数秒間ページ遷移しない)
   // 今後の動向を見て、有効にするかどうか判断
   // swcMinify: true,
   pwa: {
-    dest: "public",
+    dest: 'public',
     register: true,
     skipWaiting: true,
-    disable: process.env.NODE_ENV === "development",
+    disable: process.env.NODE_ENV === 'development',
   },
 })
