993a5ce17c646c819cd294ab25d1811fff0cda02
kusakabe <kusakabe.takuya@alleeks.com>
2022-12-25 00:18:27 +0900

    fix remove scraps sidebar

--- components/ScrapsPage/CommonScrapsPage.tsx
+++ components/ScrapsPage/CommonScrapsPage.tsx
@@ -3,12 +3,9 @@ import { Contents } from './contents/Contents'
 
 import { ScrapsType } from '../../types/microCMS/Common'
 
-import { ScrapLists } from './ScrapList'
-
 export const CommonScrapsPage = ({ dateTime, highlight, scraps }: { dateTime: string; highlight?: string; scraps: ScrapsType }) => {
   return (
     <div className="p-[24px]">
-      <ScrapLists />
       <div className="mx-[8px] w-[auto]">
         <div className="text-[32px]">{dateTime}</div>
         {highlight && <Highlight highlight={highlight} />}
