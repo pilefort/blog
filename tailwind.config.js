bb00213ba4facf6ae8784b7d9ab573adbb069d6c
kusakabe <kusakabe.takuya@alleeks.com>
2023-01-21 11:39:31 +0900

    fix tailwindcssの適用ファイルからjs, jsxを除外

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
   theme: {
     screens: {
       sm: '300px',
