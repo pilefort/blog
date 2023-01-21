794e23651ba12bb08eb2fcffdeb74dc49085319c
kusakabe <kusakabe.takuya@alleeks.com>
2023-01-21 11:58:39 +0900

    fix useMediaQueryをHeaderコンポーネントに統合しました

--- components/Layouts/Header.tsx
+++ components/Layouts/Header.tsx
@@ -1,9 +1,22 @@
 import { DesktopHeader } from '@components/Layouts/Header/DesktopHeader'
 import { SPHeader } from '@components/Layouts/Header/SPHeader'
-import { useMediaQuery } from '../../hooks/useMediaQuery'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 
 export const Header = () => {
-  const isDesktop = useMediaQuery()
+  const [isDesktop, setIsDesktop] = useState(true)
+  const router = useRouter()
+
+  useEffect(() => {
+    if (!router.isReady) return
+
+    const checkIsDesktop = () => {
+      window.innerWidth >= 900 ? setIsDesktop(true) : setIsDesktop(false)
+    }
+
+    window.addEventListener('resize', checkIsDesktop)
+    return () => window.removeEventListener('resize', checkIsDesktop)
+  }, [router.isReady])
 
   return <>{isDesktop ? <DesktopHeader /> : <SPHeader />}</>
 }
