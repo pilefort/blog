80101ff955bcef421401f9c27341e669a6393095
kusakabe <kusakabe.takuya@alleeks.com>
2023-05-21 21:17:41 +0900

    feat サンプル追加

--- components/WorksPage/Layouts/IndexPage.tsx
+++ components/WorksPage/Layouts/IndexPage.tsx
@@ -1,27 +1,33 @@
 import { Card } from '@components/WorksPage/Card'
-import tech14 from '@public/assets/tech-dojin/tech14.jpg'
-import tech13 from '@public/assets/tech-dojin/tech13.jpg'
-import tech12 from '@public/assets/tech-dojin/tech12.jpg'
+import WebChangelog2023part1 from '@public/assets/tech_dojin/web_changelog_2023part1.jpg'
+import WebChangelog2022part2 from '@public/assets/tech_dojin/web_changelog_2022part2.jpg'
+import WebChangelog2022part1 from '@public/assets/tech_dojin/web_changelog_2022part1.jpg'
+import WebChangelog2021 from '@public/assets/tech_dojin/web_changelog_2021.jpg'
 
 export const IndexPage = () => {
   return (
     <div className="m-auto grid w-fit md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
       <Card
-        src={'/works'}
-        body={'2022年後半に登場したライブラリ・サービス・フレームワークについて紹介する本 (準備中)'}
-        img={tech14}
+        src={'/samples/MyWebChangelog2023part1.pdf'}
+        body={'2023年前半に登場したライブラリ・サービス・フレームワークについて紹介する本 (準備中)'}
+        img={WebChangelog2023part1}
         priority={true}
-        linkDisabled={true}
+      />
+      <Card
+        src={'/samples/MyWebChangelog2022part2.pdf'}
+        body={'2022年後半に登場したライブラリ・サービス・フレームワークについて紹介する本 (準備中)'}
+        img={WebChangelog2022part2}
+        priority={true}
       />
       <Card
         src={'/works/web_changelog_2022part1/preface'}
         body={'2022年前半に登場したライブラリ・サービス・フレームワークについて紹介する本'}
-        img={tech13}
+        img={WebChangelog2022part1}
       />
       <Card
         src={'/works/web_changelog_2021/preface'}
         body={'2021年に登場したライブラリ・サービス・フレームワークについて紹介する本'}
-        img={tech12}
+        img={WebChangelog2021}
       />
     </div>
   )
