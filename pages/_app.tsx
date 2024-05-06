33e42d2e605a2fc0f6328c4f44c8fb9a74e66083
kusakabe <kusakabe.takuya@alleeks.com>
2024-05-06 22:36:36 +0900

    posthog test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,19 @@ import Script from 'next/script'
 
 import { Header } from '@components/Layouts/Header'
 import { Footer } from '@components/Layouts/Footer'
+import posthog from "posthog-js"
+import { PostHogProvider } from 'posthog-js/react'
 
 import type { AppProps } from 'next/app'
+
+if (typeof window !== 'undefined') { // checks that we are client-side
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === 'development') posthog.debug() // debug mode in development
+    },
+  })
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -29,7 +40,9 @@ function MyApp({ Component, pageProps }: AppProps) {
       <>
         <Header />
         <div className="min-h-screen">
-          <Component {...pageProps} />
+          <PostHogProvider client={posthog}>
+            <Component {...pageProps} />
+          </PostHogProvider>
         </div>
       </>
       <Footer />
