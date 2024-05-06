cb73570a8e07416bc9536be6c549ccd4d0f51514
kusakabe <kusakabe.takuya@alleeks.com>
2024-05-06 22:46:12 +0900

    fix lint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,14 @@ import Script from 'next/script'
 
 import { Header } from '@components/Layouts/Header'
 import { Footer } from '@components/Layouts/Footer'
-import posthog from "posthog-js"
+import posthog from 'posthog-js'
 import { PostHogProvider } from 'posthog-js/react'
 
 import type { AppProps } from 'next/app'
 
-if (typeof window !== 'undefined') { // checks that we are client-side
-  posthog.init("phc_WYnf7GWtk4JERgIl29UUD6STVXAYRVSOeEzZpJJ9H20", {
+if (typeof window !== 'undefined') {
+  // checks that we are client-side
+  posthog.init('phc_WYnf7GWtk4JERgIl29UUD6STVXAYRVSOeEzZpJJ9H20', {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
     loaded: (posthog) => {
       if (process.env.NODE_ENV === 'development') posthog.debug() // debug mode in development
